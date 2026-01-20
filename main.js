b34fbbd07abd342c350eccaab3e325505ab9ca98
Simon Ravnic <simon@MacBook-Pro-Simon.local>
2026-01-20 14:49:36 +0100

    Remove cty/master upload buttons

--- main.js
+++ main.js
@@ -52,7 +52,7 @@
     { id: 'sh6_info', title: 'SH6 info' }
   ];
 
-  const APP_VERSION = 'v0.5.13';
+  const APP_VERSION = 'v0.5.14';
   const CORS_PROXIES = [
     (url) => `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`,
     (url) => `https://corsproxy.io/?${encodeURIComponent(url)}`

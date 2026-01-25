e207400093a18b57babe594fed827ad5934adadf
s53zo <44831766+s53zo@users.noreply.github.com>
2026-01-25 22:54:06 +0100

    Add SH6 logo and favicon

--- main.js
+++ main.js
@@ -54,7 +54,7 @@
     { id: 'sh6_info', title: 'SH6 info' }
   ];
 
-  const APP_VERSION = 'v2.1.35';
+  const APP_VERSION = 'v2.1.36';
   const SQLJS_BASE_URLS = [
     'https://cdn.jsdelivr.net/npm/sql.js@1.8.0/dist/',
     'https://unpkg.com/sql.js@1.8.0/dist/'
